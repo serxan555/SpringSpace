0e9077afe9becc4021481f1b5a48ff95ba030cfc
serxan555 <serxan555>
2019-08-27 17:10:13 +0400

    SpringSpace

---
+++ SpringRestJersey/src/com/ss/Center.java
@@ -0,0 +1,14 @@
+package com.ss;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+// http://localhost:8080/SpringRestJersey/rest/main
+@Path(value = "/main")
+public class Center {
+
+    @GET
+    public String name() {
+        return "SS.555";
+    }
+}
